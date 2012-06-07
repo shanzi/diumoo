a3fa0d219960081ecabe88dc34fd358553e940f6
shanzi <yun.er.run@gmail.com>
2012-06-07 21:35:10 +0800

    Initial Commit

---
+++ diumoo/DMDocument.h
@@ -0,0 +1,13 @@
+//
+//  DMDocument.h
+//  diumoo
+//
+//  Created by Shanzi on 12-6-7.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface DMDocument : NSDocument
+
+@end
---
+++ diumoo/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  diumoo
+//
+//  Created by Shanzi on 12-6-7.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
