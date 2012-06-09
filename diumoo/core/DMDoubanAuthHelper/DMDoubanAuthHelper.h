96c7b46def618b6dc5bf7fb8c64415396e9bab4d
shanzi <yun.er.run@gmail.com>
2012-06-09 18:49:38 +0800

    添加登陆用helper

---
+++ diumoo/core/DMDoubanAuthHelper/DMDoubanAuthHelper.h
@@ -0,0 +1,13 @@
+//
+//  DMDoubanAuthHelper.h
+//  diumoo
+//
+//  Created by Shanzi on 12-6-9.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DMDoubanAuthHelper : NSObject
+
+@end
---
+++ diumoo/core/DMDoubanAuthHelper/DMDoubanAuthHelper.m
@@ -0,0 +1,13 @@
+//
+//  DMDoubanAuthHelper.m
+//  diumoo
+//
+//  Created by Shanzi on 12-6-9.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "DMDoubanAuthHelper.h"
+
+@implementation DMDoubanAuthHelper
+
+@end
