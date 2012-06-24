7dc0510efe9f06f6d8afbdda7f2235e5d910eca4
shanzi <yun.er.run@gmail.com>
2012-06-24 23:00:57 +0800

    添加自定义的Volume cell 样式

---
+++ diumoo/ui/DMPanel/DMVolumeSlideCell/DMVolumeSlideCell.h
@@ -0,0 +1,13 @@
+//
+//  DMVolumeSlideCell.h
+//  diumoo
+//
+//  Created by Shanzi on 12-6-24.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface DMVolumeSlideCell : NSSliderCell
+
+@end
---
+++ diumoo/ui/DMPanel/DMVolumeSlideCell/DMVolumeSlideCell.m
@@ -0,0 +1,61 @@
+//
+//  DMVolumeSlideCell.m
+//  diumoo
+//
+//  Created by Shanzi on 12-6-24.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "DMVolumeSlideCell.h"
+
+@implementation DMVolumeSlideCell
+
+
+-(void) drawBarInside:(NSRect)aRect flipped:(BOOL)flipped
+{
+    NSBezierPath* bar = [NSBezierPath bezierPathWithRect:aRect];
+    [[NSColor colorWithGenericGamma22White:0.9 alpha:1.0] setFill];
+    [bar fill];
+}
+
+-(void) drawKnob:(NSRect)knobRect
+{
+    CGFloat midY = NSMidY(knobRect);
+    CGFloat maxX = knobRect.origin.x + knobRect.size.width;
+    
+    NSRect knobR = NSMakeRect(0, 0, maxX, knobRect.size.height);
+    
+    NSBezierPath* knob = [NSBezierPath bezierPathWithRect:knobR];
+    
+    
+    NSColor* color = [NSColor colorWithSRGBRed:0.4 green:0.8 blue:1.0 alpha:0.8];
+    [color setFill];
+    [knob fill];
+    
+    
+   
+    
+    // 绘制声音图像
+    double rate = (self.floatValue - self.minValue)/self.maxValue;
+    NSImage* image =nil;
+    if (rate > 0.6) {
+        image = [NSImage imageNamed:@"sound_high.png"];
+    }
+    else if(rate > 0.1)
+    {
+        image = [NSImage imageNamed:@"sound_low.png"];
+    }
+    else {
+        image = [NSImage imageNamed:@"sound_mute.png"];
+    }
+    
+    NSRect fromRect = NSMakeRect(0, 0, image.size.width, image.size.height);
+    NSRect toRect = NSMakeRect(5 , midY-8,
+                               16, 16);
+    [image drawInRect:toRect
+             fromRect: fromRect
+            operation:NSCompositeSourceOver
+             fraction:1.0];
+}
+
+@end
