8708df8de835621c3fcb3f5642665712273b8dbf
shanzi <yun.er.run@gmail.com>
2012-07-02 23:35:31 +0800

    添加playrecordhandler

---
+++ diumoo/core/DMPlayRecordHandler/DMPlayRecordHandler.h
@@ -0,0 +1,13 @@
+//
+//  DMPlayRecordHandler.h
+//  diumoo
+//
+//  Created by Shanzi on 12-7-2.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DMPlayRecordHandler : NSObject
+
+@end
---
+++ diumoo/core/DMPlayRecordHandler/DMPlayRecordHandler.m
@@ -0,0 +1,22 @@
+//
+//  DMPlayRecordHandler.m
+//  diumoo
+//
+//  Created by Shanzi on 12-7-2.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "DMPlayRecordHandler.h"
+
+@implementation DMPlayRecordHandler
+
+-(id) init
+{
+    self = [super init];
+    if (self) {
+        
+    }
+    return self;
+}
+
+@end
