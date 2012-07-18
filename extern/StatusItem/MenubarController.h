26747d45a75f46a12cc4f23ecc6c1d52ae93f8bc
shanzi <yun.er.run@gmail.com>
2012-07-19 00:11:03 +0800

    添加statusitem控制

---
+++ extern/StatusItem/MenubarController.h
@@ -0,0 +1,17 @@
+#define STATUS_ITEM_VIEW_WIDTH 24.0
+
+#pragma mark -
+
+@class StatusItemView;
+
+@interface MenubarController : NSObject {
+@private
+    StatusItemView *_statusItemView;
+}
+
+@property (nonatomic) BOOL hasActiveIcon;
+@property (nonatomic, assign, readonly) StatusItemView *statusItemView;
+
+- (NSStatusItem *)statusItem;
+-(void) setAction:(SEL) action withTarget:(id) target;
+@end
