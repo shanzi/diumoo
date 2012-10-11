3ff5b6fc25f903679b96129d867f92fa5c962008
shanzi <yun.er.run@gmail.com>
2012-10-11 22:15:26 +0800

    search window

---
+++ diumoo/ui/DMSearchPanel/DMSearchPanelControllerWindowController.h
@@ -0,0 +1,13 @@
+//
+//  DMSearchPanelControllerWindowController.h
+//  diumoo
+//
+//  Created by Shanzi on 12-10-9.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface DMSearchPanelControllerWindowController : NSWindowController
+
+@end
---
+++ diumoo/ui/DMSearchPanel/DMSearchPanelControllerWindowController.m
@@ -0,0 +1,34 @@
+//
+//  DMSearchPanelControllerWindowController.m
+//  diumoo
+//
+//  Created by Shanzi on 12-10-9.
+//
+//
+
+#import "DMSearchPanelControllerWindowController.h"
+
+@interface DMSearchPanelControllerWindowController ()
+
+@end
+
+@implementation DMSearchPanelControllerWindowController
+
+- (id)initWithWindow:(NSWindow *)window
+{
+    self = [super initWithWindow:window];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)windowDidLoad
+{
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
