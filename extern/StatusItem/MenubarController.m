b71d8b4bd1673b43ff94918f3160c6f1bce3ff90
Chase Zhang <yun.er.run@gmail.com>
2016-06-12 21:10:44 +0800

    Adding status icons for dark mode

--- extern/StatusItem/MenubarController.m
+++ extern/StatusItem/MenubarController.m
@@ -9,21 +9,45 @@
 
 - (id)init
 {
-    if (self = [super init])
-    {
-        // Install status item into the menu bar
-        NSStatusItem *statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:STATUS_ITEM_VIEW_WIDTH];
-        _statusItemView = [[StatusItemView alloc] initWithStatusItem:statusItem] ;
-        _statusItemView.image = [NSImage imageNamed:@"status-icon"];
-        _statusItemView.mixedImage = [NSImage imageNamed:@"status-icon-mixed"];
-        _statusItemView.alternateImage = [NSImage imageNamed:@"status-icon-alt"];
-    }
-    return self;
+	if (self = [super init])
+	{
+		// Install status item into the menu bar
+		NSStatusItem *statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:STATUS_ITEM_VIEW_WIDTH];
+		_statusItemView = [[StatusItemView alloc] initWithStatusItem:statusItem];
+		
+		[self setStatusItemImage];
+		
+		[[NSDistributedNotificationCenter defaultCenter]
+		 addObserver:self
+		 selector:@selector(setStatusItemImage)
+		 name:@"AppleInterfaceThemeChangedNotification"
+		 object:nil];
+	}
+	return self;
+}
+
+- (bool) isDarkMode
+{
+	NSString * interfaceStyle = [[NSUserDefaults standardUserDefaults] stringForKey: @"AppleInterfaceStyle"];
+	return (interfaceStyle != nil) && [interfaceStyle isEqualToString:@"Dark"];
+}
+
+- (void) setStatusItemImage
+{
+	if ([self isDarkMode]) {
+		_statusItemView.image = [NSImage imageNamed:@"dark-status-icon"];
+		_statusItemView.mixedImage = [NSImage imageNamed:@"dark-status-icon-mixed"];
+		_statusItemView.alternateImage = [NSImage imageNamed:@"dark-status-icon-alt"];
+	} else {
+		_statusItemView.image = [NSImage imageNamed:@"status-icon"];
+		_statusItemView.mixedImage = [NSImage imageNamed:@"status-icon-mixed"];
+		_statusItemView.alternateImage = [NSImage imageNamed:@"status-icon-alt"];
+	}
 }
 
 - (void)dealloc
 {
-    [[NSStatusBar systemStatusBar] removeStatusItem:self.statusItem];
+	[[NSStatusBar systemStatusBar] removeStatusItem:self.statusItem];
 }
 
 #pragma mark -
@@ -31,30 +55,30 @@
 
 - (NSStatusItem *)statusItem
 {
-    return self.statusItemView.statusItem;
+	return self.statusItemView.statusItem;
 }
 
 -(void) setMixed:(BOOL)mixed
 {
-    [_statusItemView setMixed:mixed];
+	[_statusItemView setMixed:mixed];
 }
 
 -(void) setAction:(SEL)action withTarget:(id)target
 {
-    _statusItemView.action = action;
-    _statusItemView.target = target;
+	_statusItemView.action = action;
+	_statusItemView.target = target;
 }
 
 #pragma mark -
 
 - (BOOL)hasActiveIcon
 {
-    return self.statusItemView.isHighlighted;
+	return self.statusItemView.isHighlighted;
 }
 
 - (void)setHasActiveIcon:(BOOL)flag
 {
-    self.statusItemView.isHighlighted = flag;
+	self.statusItemView.isHighlighted = flag;
 }
 
 @end
